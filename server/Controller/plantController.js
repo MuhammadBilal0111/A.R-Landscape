c830a4d38dff194dc332abb049516927552af4f9
Muhammad Bilal <m.bilal0111@gmail.com>
2024-11-18 18:43:28 -0800

    create get all plants api

--- server/Controller/plantController.js
+++ server/Controller/plantController.js
@@ -1,8 +1,12 @@
 const PlantModel = require("../Model/plantModel");
 const asyncErrorHandler = require("../utils/asyncErrorHandler");
-exports.getPlants = async () => {
-  console.log("luv u");
-};
+exports.getPlants = asyncErrorHandler(async (req, res, next) => {
+  const plantData = await PlantModel.find().sort({ dateAdded: -1 }); // -1 for desc
+  res.status(200).json({
+    status: "success",
+    data: plantData,
+  });
+});
 exports.addPlant = asyncErrorHandler(async (req, res, next) => {
   const plantData = await PlantModel.create(req.body);
   res.status(200).json({
